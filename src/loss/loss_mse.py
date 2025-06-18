4e7c1cab883fb3a8afaae2100ef7870260c07a88
marcel <tgueloglu@ethz.ch>
2025-06-18 13:45:25 +0200

    Add mse loss clamping

--- src/loss/loss_mse.py
+++ src/loss/loss_mse.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+import torch
 from jaxtyping import Float
 from torch import Tensor
 
@@ -12,6 +13,9 @@ from .loss import Loss
 @dataclass
 class LossMseCfg:
     weight: float
+    l1_loss: bool = False
+    clamp_large_error: float = 0.1
+    clamp_after_step: int = 1000
 
 
 @dataclass
@@ -28,4 +32,12 @@ class LossMse(Loss[LossMseCfg, LossMseCfgWrapper]):
         global_step: int,
     ) -> Float[Tensor, ""]:
         delta = prediction.color - batch["target"]["image"]
+        if self.cfg.clamp_large_error > 0 and global_step >= self.cfg.clamp_after_step:
+            valid_mask = (delta ** 2) < self.cfg.clamp_large_error
+            delta = delta[valid_mask]
+            # If no pixels are valid, return a very small loss
+            if delta.numel() == 0:
+                return torch.tensor(1e-6, dtype=torch.float32, device=delta.device)
+        if self.cfg.l1_loss:
+            return self.cfg.weight * (delta.abs()).mean()
         return self.cfg.weight * (delta**2).mean()
